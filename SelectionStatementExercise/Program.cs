9ac16f5257d038b2024d12680d56e3bdd4ead36a
Ian Simpson <ianbsimpson@gmail.com>
2022-10-19 12:46:48 -0700

    first commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //var favNumber = 2; 
+
+            var r = new Random();
+            var favNumber = r.Next(1,10);
+
+            Console.WriteLine("Try to guess my favorite number between 1 & 10."); 
+            
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+                Console.WriteLine("You got it!!!");
+
+            if (userInput < favNumber)
+                Console.WriteLine("Too low.");
+
+            else if (userInput > favNumber)
+                Console.WriteLine("Too high.");
+
+            else Console.WriteLine("nevermind.");
         }
     }
 }
