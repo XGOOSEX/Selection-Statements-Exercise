e2f85692127bc0630bb497b673252087ceb33c23
Ian Simpson <ianbsimpson@gmail.com>
2022-10-20 07:39:51 -0700

    first commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -9,23 +9,53 @@ namespace SelectionStatementExercise
 
             //var favNumber = 2; 
 
-            var r = new Random();
-            var favNumber = r.Next(1,10);
+            //var r = new Random();
+            //var favNumber = r.Next(1,10);
 
-            Console.WriteLine("Try to guess my favorite number between 1 & 10."); 
+            //Console.WriteLine("Try to guess my favorite number between 1 & 10."); 
+
+            //var userInput = int.Parse(Console.ReadLine());
+
+            //if (userInput == favNumber)
+            //    Console.WriteLine("You got it!!!");
+
+            //if (userInput < favNumber)
+            //    Console.WriteLine("Too low.");
+
+            //else if (userInput > favNumber)
+            //    Console.WriteLine("Too high.");
+
+            //else Console.WriteLine("nevermind.");
+
+            Console.WriteLine("What is your favorite school subject?");
+
+            String favSubject = Console.ReadLine().ToLower();
             
-            var userInput = int.Parse(Console.ReadLine());
+            switch(favSubject)
+            {
+                case "history":
+                    Console.WriteLine("Fun!!!");
+                    break;
+                case "math":
+                    Console.WriteLine("Nerd....");
+                    break;
+                case "science":
+                    Console.WriteLine("Bill Nye would be proud.");
+                    break;
+                case "english":
+                    Console.WriteLine("Shakespear would be proud. ");
+                    break;
+                case "pe":
+                    Console.WriteLine("You are a bro. ");
+                    break;
 
-            if (userInput == favNumber)
-                Console.WriteLine("You got it!!!");
 
-            if (userInput < favNumber)
-                Console.WriteLine("Too low.");
 
-            else if (userInput > favNumber)
-                Console.WriteLine("Too high.");
-
-            else Console.WriteLine("nevermind.");
+            }
+            
         }
+
+
+
     }
 }
